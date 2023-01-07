7d699fe76c292a4ed8573db3f0fc9ff6528d6630
Osama Ansari <ansariosama192@gmail.com>
2023-01-07 19:26:03 +0530

    07-01-2023

---
+++ Loops/aRaiseToB.java
@@ -0,0 +1,20 @@
+// WAP to find A raise to the power B.
+
+package Loops;
+
+import java.util.Scanner;
+
+public class aRaiseToB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Value of a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the Value of b: ");
+        int b = sc.nextInt();
+        int power = 1;
+        for (int i = 1; i <= b; i++) {
+            power = power * a;
+        }
+        System.out.println("Power of " + a + "^" + b + " is " + power);
+    }
+}
---
+++ Loops/firstNFactorial.java
@@ -0,0 +1,19 @@
+// WAP to print First N Factorial Number.
+
+package Loops;
+
+import java.util.Scanner;
+
+public class firstNFactorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Value of N : ");
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+            System.out.println("Factorial of " + i + " : " + fact);
+        }
+        sc.close();
+    }
+}
