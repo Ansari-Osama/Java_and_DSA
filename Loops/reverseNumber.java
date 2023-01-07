915727d35412bb4e8b2c2db40f96a79b7f7f8d19
Osama Ansari <ansariosama192@gmail.com>
2023-01-07 18:13:58 +0530

    07-01-2023

---
+++ Loops/reverseNumber.java
@@ -0,0 +1,20 @@
+// WAP to Reverse a Given Number.
+
+package Loops;
+
+import java.util.Scanner;
+
+public class reverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int n = sc.nextInt();
+        int originalN = n, reverse = 0;
+        while (n != 0) {
+            int lastDigit = n % 10;
+            reverse = reverse * 10 + lastDigit;
+            n = n / 10;
+        }
+        System.out.println("The Reverse of " + originalN + " is " + reverse);
+    }
+}
---
+++ Loops/sumOfDigits.java
@@ -0,0 +1,20 @@
+// WAP to print Sum of Digits of a Number.
+
+package Loops;
+
+import java.util.Scanner;
+
+public class sumOfDigits {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = sc.nextInt();
+        int originalN = n, sum = 0;
+        while (n != 0) {
+            int lastDigit = n % 10;
+            sum += lastDigit;
+            n = n / 10;
+        }
+        System.out.println("Sum of Digits of " + originalN + " is " + sum);
+    }
+}
